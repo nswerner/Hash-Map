d4c5f345b78a8f646d589c0ec0cb1ed7bc6e3a38
App Academy Student <aa@Apps-Mac-mini.local>
2019-02-15 14:17:56 -0500

    Completed Array Hash function

--- Hash_Map/skeleton/lib/p02_hashing.rb
+++ Hash_Map/skeleton/lib/p02_hashing.rb
@@ -4,6 +4,38 @@ end
 
 class Array
   def hash
+    string = self.stringify_array
+    codified_string = string.chars.map(&:ord)
+    large_num = codified_string.inject { |accum, el| accum.to_s + el.to_s }.to_i
+    binary = large_num.to_s(2)
+    flatten_binary(binary)
+  end
+  
+
+  def stringify_array
+    string = "["
+    self.each do |element|
+      if element.is_a?(Array)
+        string += element.stringify_array
+      else
+        string += element.to_s
+      end
+    end
+
+    string += "]"
+  end
+
+  def flatten_binary(string)
+    segment = string[0..13].to_i
+    rest = string[14..-1]
+
+    until rest.nil? || rest.empty?
+      new_segment = rest[0..13].to_i
+      segment += new_segment
+      rest = rest[14..-1]
+    end
+  
+    segment
   end
 end
 
@@ -19,3 +51,6 @@ class Hash
     0
   end
 end
+
+
+#   load  "lib/p02_hashing.rb"
