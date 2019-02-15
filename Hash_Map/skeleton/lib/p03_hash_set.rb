97c45a2c3d77e02e14b0a407e0b12537bdacde66
App Academy Student <aa@Apps-Mac-mini.local>
2019-02-15 17:02:55 -0500

    Create linked list

--- Hash_Map/skeleton/lib/p03_hash_set.rb
+++ Hash_Map/skeleton/lib/p03_hash_set.rb
@@ -1,5 +1,6 @@
 class HashSet
-  attr_reader :count
+  # attr_reader :count, :store
+  attr_accessor :store, :count
 
   def initialize(num_buckets = 8)
     @store = Array.new(num_buckets) { Array.new }
@@ -7,24 +8,43 @@ class HashSet
   end
 
   def insert(key)
+    unless include?(key)
+      self[key] << key
+      self.count += 1
+      resize! if store.length < count
+      true
+    end
   end
 
   def include?(key)
+    self[key].include?(key) 
   end
 
   def remove(key)
+    if include?(key)
+      self[key].delete(key)
+      self.count -= 1
+    end
   end
 
   private
-
   def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+   store[num.hash % num_buckets] 
   end
 
   def num_buckets
-    @store.length
+    store.length
   end
 
   def resize!
+    new_store = Array.new(num_buckets * 2) { Array.new }
+    
+    store.each do |bucket|
+      bucket.each do |element|
+        new_store[element.hash % (num_buckets * 2)] << element
+      end
+    end
+
+    self.store = new_store
   end
 end
--- Hash_Map/skeleton/lib/p04_linked_list.rb
+++ Hash_Map/skeleton/lib/p04_linked_list.rb
@@ -1,3 +1,5 @@
+require 'byebug'
+
 class Node
   attr_reader :key
   attr_accessor :val, :next, :prev
@@ -20,7 +22,15 @@ class Node
 end
 
 class LinkedList
+  include Enumerable
+
+  attr_accessor :head, :tail
+
   def initialize
+    @head = Node.new
+    @tail = Node.new
+    @head.next = @tail
+    @tail.prev = @head
   end
 
   def [](i)
@@ -29,34 +39,79 @@ class LinkedList
   end
 
   def first
+    return nil if empty?
+    @head.next
   end
-
+  
   def last
+    return nil if empty?
+    @tail.prev
   end
 
   def empty?
+    head.next == tail
   end
 
   def get(key)
+    each do |link|
+      return link.val if link.key == key
+    end
+
+    nil
   end
 
   def include?(key)
+    each do |link|
+      return true if link.key == key
+    end
+
+    false
   end
 
   def append(key, val)
+    new_node = Node.new(key, val)
+    new_node.next = tail
+    new_node.prev = tail.prev
+
+    tail.prev = new_node
+    new_node.prev.next = new_node
   end
 
   def update(key, val)
+    each do |link|
+      link.val = val if link.key == key
+    end
   end
 
   def remove(key)
+    return nil unless include?(key)
+    removed_node = nil
+    each do |link|
+      removed_node = link if link.key == key
+    end
+
+    tail_side = removed_node.next
+    head_side = removed_node.prev
+  
+    tail_side.prev = head_side
+    head_side.next = tail_side
+
+    removed_node
   end
 
-  def each
+  def each(&prc)
+
+    next_link = first
+    until next_link == tail || next_link.nil?
+      prc.call(next_link)
+      next_link = next_link.next
+    end
+
+    self
   end
 
   # uncomment when you have `each` working and `Enumerable` included
-  # def to_s
-  #   inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
-  # end
+  def to_s
+    inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
+  end
 end
